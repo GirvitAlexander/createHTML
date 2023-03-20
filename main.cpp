6d2936c281c644761a6b351a8d0a9d76f03e5e51
GirvitAlexander <girvits.alexander@gmail.com>
2023-03-20 22:23:59 +0300

    Add 'empty' element HTML. Change comments

--- main.cpp
+++ main.cpp
@@ -19,23 +19,28 @@ namespace {
     constexpr int MAX_RECEIPT = 100'000; 
 }
 
+/**
+ * @brief Class for node(element) to HTML
+ **/
 class Node {
 public:
-    explicit Node(string _name, string _value = "") : name(_name), value(_value) {}
+    explicit Node(string _name, bool _isEmptyNode = false, string _value = "") : name(_name), isEmptyNode(_isEmptyNode), value(_value) {}
 
-    /* Function for adding child to node
-     * newNode - new child for node
-     * return: nothing
-     */
+    /**
+     * @brief Add child to node
+     * @param newNode - new child for node
+     * @return void
+     **/
     void addNode(std::shared_ptr<Node> newNode) {
         newNode->depth = depth + 1;
         arrayNodes.push_back(newNode);
     }
 
-    /* Function for getting child node by index
-     * index - index array children
-     * return: shared pointer to child node
-     */
+    /**
+     * @brief Get child node by index
+     * @param index - index array children
+     * @return shared pointer to child node
+     **/
     std::shared_ptr<Node> getNode(size_t index) const {
         if (index >= arrayNodes.size()) {
             throw std::logic_error("Index out of array");
@@ -43,20 +48,23 @@ public:
         return arrayNodes[index];
     }
 
-    /* Function for get count of children node
-     * return: size of array children
-     */
+    /**
+     * @brief Get count of children node
+     * @return size of array children
+     **/
     size_t getSize() const {
         return arrayNodes.size();
     }
 
-    /* Function for setting tag for node
-     * nameTag - name tag node
-     * valueTag - value this tag
-     * return: nothing
-     */
-    void setTag(string nameTag, string valueTag) {
-        tags[nameTag] = valueTag;
+    /**
+     * @brief setting attribute for node
+     * @param nameAttr - name tag node
+     * @param valueAttr - value this attribute
+     * @return * void 
+     *
+     **/
+    void setAttr(string nameAttr, string valueAttr) {
+        attrs[nameAttr] = valueAttr;
     }
     
     friend std::ostream& operator<<(std::ostream& fs, const Node& node);
@@ -64,42 +72,49 @@ public:
 private:
     string name;
     size_t depth = 0;
+    bool isEmptyNode;
     string value = "";
     vector<std::shared_ptr<Node>> arrayNodes;
 
-    std::unordered_map<string, string> tags;
+    std::unordered_map<string, string> attrs;
 };
 
-/* Operator for output in file node
- * fs - file stream, where the information about the node is output
- * node - node that is being output
- * return: file stream
+/**
+ * @brief Operator for output in file node
+ * 
+ * @param fs - file stream, where the information about the node is output
+ * @param node - node that is being output
+ * @return std::ostream& - file stream
  */
 std::ostream& operator<<(std::ostream& fs, const Node& node) {
     string tab(node.depth, '\t');
 
     fs << tab << "<" << node.name;
 
-    for (const auto& [key, value]: node.tags) {
+    for (const auto& [key, value]: node.attrs) {
         fs << " " << key << "=\"" << value << "\"";
     }
     fs << ">\n";
-    if (node.value != "") {
-        fs << tab << "\t" << node.value << "\n";
-    }
-    for (const auto& child: node.arrayNodes) {
-        fs << *child;
-    }
 
-    fs << tab << "</" << node.name << ">\n";
+    if (!node.isEmptyNode) {
+        if (node.value != "") {
+            fs << tab << "\t" << node.value << "\n";
+        }
+        for (const auto& child: node.arrayNodes) {
+            fs << *child;
+        }
 
+        fs << tab << "</" << node.name << ">\n";
+    }
     return fs;
 }
 
-/* Function for creating value for report table
- * table - table that is filled with value
- * nInitBalance - elementary balance
- * return: nothing
+/**
+ * @brief Create table for report
+ * 
+ * @param table - table that is filled with value
+ * @param nInitBalance - elementary balance
+ * @return void
  */
 void CreateTable(vector<vector<int>>& table, int nInitBalance) {
     int prevBalance = nInitBalance, receipt = 0, expenditure = 0;
@@ -116,25 +131,31 @@ void CreateTable(vector<vector<int>>& table, int nInitBalance) {
 
         row[1] = receipt;
         row[2] = expenditure;
+
     }
 
 }
 
-/* Function for creating HTML tree for file
- * table - table with value
- * rootNode - root node with name <html>
- * return: nothing
+/**
+ * @brief  Creating HTML tree for file
+ * 
+ * @param table - table with values
+ * @param rootNode - root node with name <html>
+ * @return void
  */
 void CreateHTML(const vector<vector<int>>& table, std::shared_ptr<Node> rootNode) {
-    rootNode->addNode(std::make_shared<Node>("head"));
-    rootNode->getNode(0)->addNode(std::make_shared<Node>("title", "Report"));
+    auto headNode = std::make_shared<Node>("head");
+    rootNode->addNode(headNode);
+    headNode->addNode(std::make_shared<Node>("meta", true));
+    headNode->getNode(0)->setAttr("charset", "utf-8");
+    headNode->addNode(std::make_shared<Node>("title", false, "Report"));
 
     auto bodyNode = std::make_shared<Node>("body");
     rootNode->addNode(bodyNode);
 
     auto tableNode = std::make_shared<Node>("table");
     bodyNode->addNode(tableNode);
-    tableNode->setTag("class", "report_table");
+    tableNode->setAttr("class", "report_table");
 
     tableNode->addNode(std::make_shared<Node>("thead"));
     tableNode->addNode(std::make_shared<Node>("tbody"));
@@ -142,22 +163,24 @@ void CreateHTML(const vector<vector<int>>& table, std::shared_ptr<Node> rootNode
     // tableNode[0] = thead
     tableNode->getNode(0)->addNode(trHead);
     for (size_t i = 0; i < COLUMNS; ++i) {
-        trHead->addNode(std::make_shared<Node>("td", headerTable[i]));
+        trHead->addNode(std::make_shared<Node>("td", false, headerTable[i]));
     }
     // tableNode[1] = tbody
     for (size_t i = 0; i < ROWS; ++i) {
         tableNode->getNode(1)->addNode(std::make_shared<Node>("tr"));
-        tableNode->getNode(1)->getNode(i)->addNode(std::make_shared<Node>("td", months[i]));
+        tableNode->getNode(1)->getNode(i)->addNode(std::make_shared<Node>("td", false, months[i]));
         for (size_t j = 0; j < COLUMNS - 1; ++j) {
-            tableNode->getNode(1)->getNode(i)->addNode(std::make_shared<Node>("td", std::to_string(table[i][j + 1])));
+            tableNode->getNode(1)->getNode(i)->addNode(std::make_shared<Node>("td", false, std::to_string(table[i][j])));
         }
     }
 }
 
-/* Function for output HTML tree into file
- * rootNode - root node with name <html>
- * return: bool - true - file created successful
- *                false - file not created
+/**
+ * @brief Output HTML tree into file
+ *
+ * @param rootNode - root node with name <html>
+ * @return true - file created successful
+ * @return false - file not created
  */
 bool SaveHTMLToFile(std::shared_ptr<Node> rootNode) {
     std::fstream file("Report.htm");
@@ -172,11 +195,12 @@ bool SaveHTMLToFile(std::shared_ptr<Node> rootNode) {
     return true; 
 }
 
-/* Function for build HTML file
- * Input parameters:
- * nInitBalance - elementary balance
- * return: bool - true - file created successful
- *                false - file not created
+/**
+ * @brief Build HTML file for report
+ * 
+ * @param nInitBalance  - elementary balance
+ * @return true - file created successful
+ * @return false - file not created
  */
 bool BuildHtmlReport(int nInitBalance) {
     vector<vector<int>> table;
@@ -189,12 +213,38 @@ bool BuildHtmlReport(int nInitBalance) {
     return SaveHTMLToFile(rootNode);
 }
 
+/**
+ * @brief Main function
+ * 
+ * @param argc - count of argument
+ * @param argv - arguments value
+ * @return int
+ */
 int main(int argc, char *argv[]) {
     
-    if (BuildHtmlReport(100'000)) {
-        std::cout << "File created!!!";
+    if (argc != 2) {
+        std::cout << "Invalid input. Input example: " << argv[0] << " 100000\n";
+        return -1;
+    }
+
+    int balance = 0;
+    size_t pos = 0;
+    while (argv[1][pos] != '\0') {
+        if (std::isdigit(argv[1][pos])) {
+            balance *= 10;
+            balance += (argv[1][pos] - '0');
+            std::cout << balance << "\n";
+            ++pos;
+        } else {
+            std::cout << "Invalid input. Argument should be number!";
+            return -1;
+        }
+    }
+
+    if (BuildHtmlReport(balance)) {
+        std::cout << "File created!!!\n";
     } else {
-        std::cout << "File not created!!!";
+        std::cout << "File not created!!!\n";
     }
     return 0;
 }
